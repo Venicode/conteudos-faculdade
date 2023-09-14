69d702c07047ba464d2b02eac557daf2beb35087
Veni <44931124+Venicode@users.noreply.github.com>
2023-09-13 21:33:49 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+print("Sequencia de Fibonacci!\n")
+limite = int(input("Até quantos numeros deseja na sequencia? "))
+n1=0
+n2=1 
+n12=0
+while n12<limite:
+    print(n12)
+    n1=n2
+    n2=n12
+    n12=n1+n2
+print("fim")
+
+    
+
+    
+    
