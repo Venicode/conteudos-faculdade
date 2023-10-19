970da01cf982d36e481403c450653539e08877ba
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-19 15:43:48 -0300

    Update aulaLista.py

--- aulaLista.py
+++ aulaLista.py
@@ -1,16 +1,1 @@
-numeros = [3,2,1]
-palavras = ['teste','testando',1]
-print(numeros)
 
-numeros.append(4)
-print(numeros)
-
-listaDupla = numeros + palavras
-print(listaDupla)
-
-for elemento in numeros:
-    soma = int(elemento)+1
-    print(soma)
-
-numeros.sort()
-print(numeros)
