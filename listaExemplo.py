cae85cb88b8f680769575b05f453b4386e05fdd0
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-19 15:40:36 -0300

    Update listaExemplo.py

--- listaExemplo.py
+++ listaExemplo.py
@@ -1,26 +1,34 @@
+#criacao de listas
 lista = ['chocolate','arroz','feijao','tomate']
 numeros = [10,9,8]
 print(lista)
 
+#funcao para adicionar elemento em uma lista
 lista.append('macarrao')
 print(lista)
 
+#funcao para remover elemento de uma lista
 lista.remove('feijao')
 print(lista)
 
+#funcao para organizar a lista de forma crescente
 lista.sort()
 numeros.sort()
 print(lista)
 print(numeros)
 
+#pegando um elemento da lista pela posicao
 print(numeros[0])
 
+#juntando duas listas
 listaDupla = lista + numeros
 
 print(listaDupla)
 
+#limpando a lista
 lista.clear()
 print(lista)
 
+#organizando a lista de forma contrária
 numeros.reverse()
 print(numeros)
