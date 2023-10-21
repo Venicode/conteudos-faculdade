441af0c8a4927dbf3058e8d5f1d553f90a8ab8b8
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-21 16:30:00 +0000

    Update forca.py

--- JogodaForca/forca.py
+++ JogodaForca/forca.py
@@ -4,9 +4,42 @@ from random import choice
 placarVitorias = 0
 placarDerrotas = 0
 
+forcas = ["asciiArtes/forca0.txt", "asciiArtes/forca1.txt","asciiArtes/forca2.txt","asciiArtes/forca3.txt",
+         "asciiArtes/forca4.txt","asciiArtes/forca5.txt","asciiArtes/forca6.txt","asciiArtes/forca7.txt"]
+
+artePrincipal = "asciiArtes/jogodaForca.txt"
+traco = "_"*60
+def espaco():
+    espaco = " "*55
+    return espaco
+
+def asciiArt(arte):    
+     with open (arte) as arte:
+        for linha in arte:
+            print(linha)
 while True:
-    print("Jogo da Forca")
-    arquivo = "palavras.txt"
+    asciiArt(artePrincipal)
+    print(traco,"Menu".upper(),traco)
+    listaTemas = {'1 - Jogos':'temas/jogos.txt','2 - Filmes':'temas/filmes.txt','3- Músicas':'temas/musicas.txt',
+                  '4- Tecnologia':'temas/tecnologia.txt','5- Esportes':'temas/esportes.txt','6 - Assuntos Gerais':'temas/geral.txt'}
+    for i in listaTemas.keys():
+        print(espaco(),i.upper(),espaco())
+    
+    resposta = int(input("\nEscolha um tema para jogar:".upper()))
+
+    if resposta == 1:
+        arquivo = listaTemas["1 - Jogos"]
+    if resposta == 2:
+        arquivo = listaTemas["2 - Filmes"]
+    if resposta == 3:
+        arquivo = listaTemas["3- Músicas"]
+    if resposta == 4:
+        arquivo = listaTemas["4- Tecnologia"]
+    if resposta == 5:
+        arquivo = listaTemas["5- Esportes"]
+    if resposta == 6:
+        arquivo = listaTemas["6 - Assuntos Gerais"]
+    
     palavras = []
 
     #abre o arquivo palavras.txt e lê cada linha.
@@ -18,9 +51,10 @@ while True:
     palavrasecreta = choice(palavras).upper()
     letrasC = ""
     letrasE = ""
- 
+    contador = 0
     while True:
         pista = ""
+        asciiArt(forcas[contador])
         #percorre cada letra da palavra secreta e verifica se a letra que o usuário digitou contém nela
         for letra in palavrasecreta:
             if letra in letrasC:
@@ -36,7 +70,8 @@ while True:
             break
 
         #caso o usuário erre 6 vezes as letras da palavra secreta, ele perde o jogo
-        if len(letrasE)==6:
+        if len(letrasE)==7:
+            asciiArt(forcas[7])
             print("Game over")
             print("Tentativas erradas:",len(letrasE))
             placarDerrotas+=1
@@ -56,6 +91,9 @@ while True:
         #se não, acrescenta na variável letrasE
         else:
             letrasE+=letra
+            contador +=1
+            asciiArt(forcas[contador])
+
             
 #quando o usuario acertar ou errar 6 vezes, irá aparecer a pergunta de jogar novamente
     pergunta = input("Deseja jogar novamente? Se sim digite sim, se não aperte qualquer outra tecla\n").upper().strip()
